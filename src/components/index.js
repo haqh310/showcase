a4e476b30fedb96efda7e0d1133796609dadef6c
haqh310@gmai.com <ha.le@sapotacorp.vn>
2022-10-27 15:19:51 +0700

    push code

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Download from "./Download";
+import Button from "./Button";
+import Features from "./Features";
+import SectionWrapper from "./SectionWrapper";
+
+export { SectionWrapper, Download, Button, Features };
